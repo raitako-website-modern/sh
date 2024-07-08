d8709191ad32b3cef786a8efa0d31519c79e7085
raitako-website-modern <ytengjp@gmail.com>
2024-07-08 17:40:03 +0900

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,88 @@
+# Add "add-apt-repository" command
+apt -y install software-properties-common curl apt-transport-https ca-certificates gnupg
+
+# Add additional repositories for PHP, Redis, and MariaDB
+LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
+
+# Add Redis official APT repository
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+
+# MariaDB repo setup script can be skipped on Ubuntu 22.04
+curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
+
+# Update repositories list
+apt update
+
+# Install Dependencies
+apt -y install php8.1 php8.1-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip} mariadb-server nginx tar unzip git redis-server
+
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+
+mkdir -p /var/www/pterodactyl
+cd /var/www/pterodactyl
+
+curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz
+tar -xzvf panel.tar.gz
+chmod -R 755 storage/* bootstrap/cache/
+
+mysql -u root -p
+
+# Remember to change 'yourPassword' below to be a unique password
+CREATE USER 'pterodactyl'@'127.0.0.1' IDENTIFIED BY 'yourPassword';
+CREATE DATABASE panel;
+GRANT ALL PRIVILEGES ON panel.* TO 'pterodactyl'@'127.0.0.1' WITH GRANT OPTION;
+exit
+cp .env.example .env
+composer install --no-dev --optimize-autoloader
+
+# Only run the command below if you are installing this Panel for
+# the first time and do not have any Pterodactyl Panel data in the database.
+php artisan key:generate --force
+
+php artisan p:environment:setup
+php artisan p:environment:database
+
+# To use PHP's internal mail sending (not recommended), select "mail". To use a
+# custom SMTP server, select "smtp".
+php artisan p:environment:mail
+
+php artisan migrate --seed --force
+
+php artisan p:user:make
+
+# If using NGINX or Apache (not on CentOS)
+chown -R www-data:www-data /var/www/pterodactyl/*
+
+# If using NGINX on CentOS
+chown -R nginx:nginx /var/www/pterodactyl/*
+
+# If using Apache on CentOS
+chown -R apache:apache /var/www/pterodactyl/*
+
+* * * * * php /var/www/pterodactyl/artisan schedule:run >> /dev/null 2>&1
+
+# Pterodactyl Queue Worker File
+# ----------------------------------
+
+[Unit]
+Description=Pterodactyl Queue Worker
+After=redis-server.service
+
+[Service]
+# On some systems the user and group might be different.
+# Some systems use `apache` or `nginx` as the user and group.
+User=www-data
+Group=www-data
+Restart=always
+ExecStart=/usr/bin/php /var/www/pterodactyl/artisan queue:work --queue=high,standard,low --sleep=3 --tries=3
+StartLimitInterval=180
+StartLimitBurst=30
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target
+
+sudo systemctl enable --now redis-server
+
+sudo systemctl enable --now pteroq.service
